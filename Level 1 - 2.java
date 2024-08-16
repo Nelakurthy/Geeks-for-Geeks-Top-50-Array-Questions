247b78222c4c1ca5e0440181adf199e120aff463
Nelakurthi Saptharshi Reddy <saptharshi9949@gmail.com>
2024-08-16 06:27:26 +0530

    Create Level 1 - 2.java

---
+++ Level 1 - 2.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public Pair<Long, Long> getMinMax(int[] arr) 
+    {
+        long min = arr[0];
+        long max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return new Pair<>(min, max);
+    }
+}
